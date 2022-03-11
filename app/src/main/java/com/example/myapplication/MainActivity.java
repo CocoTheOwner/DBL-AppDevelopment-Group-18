06fefd3e164aacd7bfb1396dac305fa3bd6fdd40
Marnick TOm <mjrhtom@gmail.com>
2022-03-11 14:59:44 +0100

    Added Title given on submit.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.myapplication;
 
+import static android.content.Intent.EXTRA_TEXT;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TITLE_TEXT = "com.example.myapplication.TITLE_TEXT";
     private Button submit;
 
     @Override
@@ -20,7 +23,11 @@ public class MainActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                EditText QuestionTitle = (EditText) findViewById(R.id.question_title);
+                String questionTitle = QuestionTitle.getText().toString();
+
                 Intent intent = new Intent(MainActivity.this, QuestionView.class);
+                intent.putExtra(TITLE_TEXT, questionTitle);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/myapplication/QuestionView.java
+++ app/src/main/java/com/example/myapplication/QuestionView.java
@@ -2,13 +2,24 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class QuestionView extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_view);
+
+        Intent intent = getIntent();
+        String titleText = intent.getStringExtra(MainActivity.TITLE_TEXT);
+
+        TextView titleView = (TextView) findViewById(R.id.CreateTitle);
+
+        titleView.setText(titleText);
     }
 }
