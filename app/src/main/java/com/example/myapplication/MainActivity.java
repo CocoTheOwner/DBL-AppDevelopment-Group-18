18ed00f37369c28ff9202c437ac74880f0907e75
Rafael Roane <rroanechallengera380@gmail.com>
2022-03-15 21:44:37 +0100

    Alert Dialog & Styling

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener(){
+        @Override
+        public void onClick(DialogInterface dialog, int which){
+            switch(which){
+                case DialogInterface.BUTTON_POSITIVE:
+                    //put the code here
+                    break;
+
+                case DialogInterface.BUTTON_NEGATIVE:
+                    //put code here
+                    break;
+            }
+        }
+    };
+
+    DeleteAccount.Builder builder = new AlertDialog.Builder(context);
+    builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+    .setNegativeButton("No", dialogClickListener).show();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
