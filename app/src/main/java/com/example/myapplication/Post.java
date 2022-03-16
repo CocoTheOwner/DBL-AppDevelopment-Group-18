3130a501c2c7d64305da8d8d0675e580a34b6677
robsorb <rob.schellingerhout@xs4all.nl>
2022-03-16 14:41:19 +0100

    seach ordering based on query

---
+++ app/src/main/java/com/example/myapplication/Post.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import java.util.List;
+
+import me.xdrop.fuzzywuzzy.FuzzySearch;
+
+public class Post {
+
+    private TagCollection tags;
+
+    private String title;
+
+    public Post(String title, List<String> tags) {
+        this.title = title;
+        this.tags = new TagCollection(tags);
+    }
+
+    public TagCollection getTags() {
+        return tags;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getSearchQueryScore(String query) {
+        return FuzzySearch.weightedRatio(query, title);
+    }
+}
--- app/src/main/java/com/example/myapplication/SearchResultsFragment.java
+++ app/src/main/java/com/example/myapplication/SearchResultsFragment.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
@@ -16,9 +18,16 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SearchResultsFragment extends Fragment {
+
+    private List<Post> sortedPosts;
+    private RecyclerView results;
+    private RecyclerView.Adapter resultsAdapter;
 
     public SearchResultsFragment() {
         super(R.layout.fragment_search_results);
@@ -29,14 +38,28 @@ public class SearchResultsFragment extends Fragment {
         HomePageViewModel model = new ViewModelProvider(requireActivity())
                 .get(HomePageViewModel.class);
 
+        sortedPosts = new ArrayList<>(model.getPosts());
+
         model.getSearchString().observe(getViewLifecycleOwner(), s -> {
-//            System.out.println("Search string: " + s);
+            updateSearchOrder(s);
         });
 
-        RecyclerView results = view.findViewById(R.id.search_results);
+        results = view.findViewById(R.id.search_results);
+
+        resultsAdapter = new SearchResultsRecyclerAdapter(sortedPosts);
 
         results.setLayoutManager(new LinearLayoutManager(requireActivity().getApplicationContext()));
-        results.setAdapter(new SearchResultsRecyclerAdapter(model.getPosts()));
+        results.setAdapter(resultsAdapter);
         results.setItemAnimator(new DefaultItemAnimator());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void updateSearchOrder(String query) {
+        Collections.sort(sortedPosts,
+                (a, b) -> b.getSearchQueryScore(query) - a.getSearchQueryScore(query));
+
+        resultsAdapter.notifyDataSetChanged();
+
+        System.out.println(sortedPosts.stream().map( p -> p.getTitle()).collect(Collectors.toList()));
     }
 }
